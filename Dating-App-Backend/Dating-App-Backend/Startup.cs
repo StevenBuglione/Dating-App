0dcb05a07c725a4ebdcd60219673ae7782817c5a
Steven Buglione <spbug1999@gmail.com>
2020-04-24 15:10:32 -0400

    Initial Connection of Frontend to Backend

--- Dating-App-Backend/Dating-App-Backend/Startup.cs
+++ Dating-App-Backend/Dating-App-Backend/Startup.cs
@@ -29,6 +29,7 @@ namespace Dating_App_Backend
         {
             services.AddDbContext<DataContext>(x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
             services.AddControllers();
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -42,6 +43,8 @@ namespace Dating_App_Backend
             //app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
 
             app.UseAuthorization();
 
