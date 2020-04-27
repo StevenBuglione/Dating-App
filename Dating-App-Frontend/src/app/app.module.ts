0de13957fe8224b019a9f83a50009d6b1b6085c1
Steven Buglione <spbug1999@gmail.com>
2020-04-27 17:54:13 -0400

    Front end stuff

--- Dating-App-Frontend/src/app/app.module.ts
+++ Dating-App-Frontend/src/app/app.module.ts
@@ -4,11 +4,14 @@ import { NgModule } from "@angular/core";
 import { AppComponent } from "./app.component";
 import { ValueComponent } from "./value/value.component";
 import { HttpClientModule } from "@angular/common/http";
+import { NavComponent } from "./nav/nav.component";
+import { FormsModule } from "@angular/forms";
+import { AuthService } from "./_services/auth.service";
 
 @NgModule({
-  declarations: [AppComponent, ValueComponent],
-  imports: [BrowserModule, HttpClientModule],
-  providers: [],
+  declarations: [AppComponent, ValueComponent, NavComponent],
+  imports: [BrowserModule, HttpClientModule, FormsModule],
+  providers: [AuthService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
