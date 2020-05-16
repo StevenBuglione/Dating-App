1d7dcded55285d3b7ddbbbf4308f5079cad0f66e
Steven Buglione <spbug1999@gmail.com>
2020-05-16 14:08:40 -0400

    User registration working

--- Dating-App-Frontend/src/app/app.module.ts
+++ Dating-App-Frontend/src/app/app.module.ts
@@ -2,14 +2,15 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 
 import { AppComponent } from "./app.component";
-import { ValueComponent } from "./value/value.component";
 import { HttpClientModule } from "@angular/common/http";
 import { NavComponent } from "./nav/nav.component";
 import { FormsModule } from "@angular/forms";
 import { AuthService } from "./_services/auth.service";
+import { RegisterComponent } from "./register/register.component";
+import { HomeComponent } from "./home/home.component";
 
 @NgModule({
-  declarations: [AppComponent, ValueComponent, NavComponent],
+  declarations: [AppComponent, NavComponent, RegisterComponent, HomeComponent],
   imports: [BrowserModule, HttpClientModule, FormsModule],
   providers: [AuthService],
   bootstrap: [AppComponent],
