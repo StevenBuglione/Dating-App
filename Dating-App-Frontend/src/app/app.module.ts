f253f406edb4171e10436e6816bdab7f62c589a2
Steven Buglione <spbug1999@gmail.com>
2020-05-20 22:16:00 -0400

    routing added

--- Dating-App-Frontend/src/app/app.module.ts
+++ Dating-App-Frontend/src/app/app.module.ts
@@ -2,23 +2,37 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { HttpClientModule } from "@angular/common/http";
 import { FormsModule } from "@angular/forms";
+import { RouterModule } from "@angular/router";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 import { AppComponent } from "./app.component";
 import { NavComponent } from "./nav/nav.component";
 import { HomeComponent } from "./home/home.component";
 import { RegisterComponent } from "./register/register.component";
 import { ErrorInterceptorProvider } from "./_services/error.interceptor";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { BsDropdownModule } from "ngx-bootstrap/dropdown";
+import { MemberListComponent } from "./member-list/member-list.component";
+import { MessagesComponent } from "./messages/messages.component";
+import { ListsComponent } from "./lists/lists.component";
+import { appRoutes } from "./routes";
 
 @NgModule({
-  declarations: [AppComponent, NavComponent, HomeComponent, RegisterComponent],
+  declarations: [
+    AppComponent,
+    NavComponent,
+    HomeComponent,
+    RegisterComponent,
+    MemberListComponent,
+    MessagesComponent,
+    ListsComponent,
+  ],
   imports: [
     BrowserModule,
     HttpClientModule,
     FormsModule,
     BrowserAnimationsModule,
     BsDropdownModule.forRoot(),
+    RouterModule.forRoot(appRoutes),
   ],
   providers: [ErrorInterceptorProvider],
   bootstrap: [AppComponent],
