402426f8383e247de346b6cd5c213d65002d0462
Steven Buglione <spbug1999@gmail.com>
2020-05-23 19:43:54 -0400

    End of section 9

--- Dating-App-Frontend/src/app/app.module.ts
+++ Dating-App-Frontend/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { HttpClientModule } from "@angular/common/http";
 import { FormsModule } from "@angular/forms";
 import { RouterModule } from "@angular/router";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { JwtModule } from "@auth0/angular-jwt";
 
 import { AppComponent } from "./app.component";
 import { NavComponent } from "./nav/nav.component";
@@ -11,11 +12,24 @@ import { HomeComponent } from "./home/home.component";
 import { RegisterComponent } from "./register/register.component";
 import { ErrorInterceptorProvider } from "./_services/error.interceptor";
 import { BsDropdownModule } from "ngx-bootstrap/dropdown";
-import { MemberListComponent } from "./member-list/member-list.component";
+import { TabsModule } from "ngx-bootstrap/tabs";
+import { MemberListComponent } from "./members/member-list/member-list.component";
 import { MessagesComponent } from "./messages/messages.component";
 import { ListsComponent } from "./lists/lists.component";
 import { appRoutes } from "./routes";
+import { MemberCardComponent } from "./members/member-card/member-card.component";
+import { NgxGalleryModule } from "ngx-gallery-9";
 
+import { MemberDetailResolver } from "./_resolvers/member-detail.resolver";
+import { AlertifyService } from "./_services/alertify.service";
+import { AuthGuard } from "./_guards/auth.guard";
+import { UserService } from "./_services/user.service";
+import { MemberDetailComponent } from "./members/member-detail/member-detail.component";
+import { MemberListResolver } from "./_resolvers/member-list.resolver";
+
+export function tokenGetter() {
+  return localStorage.getItem("token");
+}
 @NgModule({
   declarations: [
     AppComponent,
@@ -23,18 +37,36 @@ import { appRoutes } from "./routes";
     HomeComponent,
     RegisterComponent,
     MemberListComponent,
-    MessagesComponent,
     ListsComponent,
+    MessagesComponent,
+    MemberCardComponent,
+    MemberDetailComponent,
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     HttpClientModule,
     FormsModule,
-    BrowserAnimationsModule,
+    NgxGalleryModule,
     BsDropdownModule.forRoot(),
+    TabsModule.forRoot(),
     RouterModule.forRoot(appRoutes),
+    JwtModule.forRoot({
+      config: {
+        tokenGetter,
+        whitelistedDomains: ["localhost:5000"],
+        blacklistedRoutes: ["localhost:5000/api/auth"],
+      },
+    }),
   ],
-  providers: [ErrorInterceptorProvider],
+  providers: [
+    ErrorInterceptorProvider,
+    MemberDetailResolver,
+    MemberListResolver,
+    AlertifyService,
+    AuthGuard,
+    UserService,
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
