f47932bbd12d443fd731ce484f987f8cc3c5b883
Steven Buglione <spbug1999@gmail.com>
2020-06-03 16:18:45 -0400

    dependancy angular

--- Dating-App-Frontend/src/app/app.module.ts
+++ Dating-App-Frontend/src/app/app.module.ts
@@ -29,6 +29,7 @@ import { MemberListResolver } from "./_resolvers/member-list.resolver";
 import { MemberEditComponent } from "./members/member-edit/member-edit.component";
 import { MemberEditResolver } from "./_resolvers/member-edit.resolver";
 import { PreventUnsavedChagnes } from "../app/_guards/prevent-unsaved-changes.guard";
+import { PhotoEditorComponent } from "./members/photo-editor/photo-editor.component";
 
 export function tokenGetter() {
   return localStorage.getItem("token");
@@ -45,6 +46,7 @@ export function tokenGetter() {
     MemberCardComponent,
     MemberDetailComponent,
     MemberEditComponent,
+    PhotoEditorComponent,
   ],
   imports: [
     BrowserModule,
---
+++ Dating-App-Frontend/src/app/members/photo-editor/photo-editor.component.ts
@@ -0,0 +1,14 @@
+import { Photo } from "./../../_models/photo";
+import { Component, OnInit, Input } from "@angular/core";
+
+@Component({
+  selector: "app-photo-editor",
+  templateUrl: "./photo-editor.component.html",
+  styleUrls: ["./photo-editor.component.css"],
+})
+export class PhotoEditorComponent implements OnInit {
+  @Input() photos: Photo[];
+  constructor() {}
+
+  ngOnInit() {}
+}
